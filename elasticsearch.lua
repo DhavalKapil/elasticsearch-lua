5c54619549dcddf4f5a24fe45ceafa2f6c40c836
Dhaval Kapil <dhavalkapil@gmail.com>
2015-03-20 19:27:42 +0530

    Added function to index a new document and modified curl_request function to allow passing data

--- elasticsearch.lua
+++ elasticsearch.lua
@@ -2,18 +2,30 @@ local http = require "socket.http"
 local url = require "socket.url"
 local table = require "table"
 local ltn12 = require "ltn12"
+local json = require "cjson"
 
 local elasticsearch = {}
 
 local Client = {}
 
-function curl_request(method, url)
+function curl_request(method, url, body)
+  body = body or nil
   local respbody = {}
-  http.request {
-    method = method,
-    url = url,
-    sink = ltn12.sink.table(respbody)
-  }
+  if body == nil then
+    http.request {
+      method = method,
+      url = url,
+      sink = ltn12.sink.table(respbody)
+    }
+  else
+    http.request {
+      method = method,
+      url = url,
+      headers = { ["Content-Length"] = string.len(body) },
+      source = ltn12.source.string(body),
+      sink = ltn12.sink.table(respbody)
+    }
+  end
   return table.concat(respbody)
 end
 
@@ -27,6 +39,21 @@ function Client:new(config)
   }
   self.__index = self
   return setmetatable(newClient, self)
+end
+
+function Client:index(document)
+  local method = ""
+  parsed_url = self.parsed_url
+  url_path = '/' .. document.index
+  url_path = url_path .. '/' .. document.type
+  if document.id ~= nil then
+    method = "PUT"
+    url_path = url_path .. '/' .. document.id
+  else
+    method = "POST"
+  end
+  parsed_url.path = url_path
+  return curl_request(method, url.build(parsed_url), json.encode(document.body))
 end
 
 function Client:delete(document)
