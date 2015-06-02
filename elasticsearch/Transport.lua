20111eab5f91193940477067b67533af568f1ed7
Dhaval Kapil <me@dhavalkapil.com>
2015-06-02 13:56:48 +0530

    Added basic code for Tranpsport module

---
+++ elasticsearch/Transport.lua
@@ -0,0 +1,34 @@
+-------------------------------------------------------------------------------
+-- Importing module
+-------------------------------------------------------------------------------
+local parser = require "parser"
+-------------------------------------------------------------------------------
+-- Declaring module
+-------------------------------------------------------------------------------
+local Transport = {}
+
+-------------------------------------------------------------------------------
+-- Declaring instance variables
+-------------------------------------------------------------------------------
+
+-- The ConnectionPool instance
+Transport.connectionPool = nil
+
+-------------------------------------------------------------------------------
+-- Returns the next connection from the ConnectionPool
+--
+-- @return  table   An instance of Connection
+-------------------------------------------------------------------------------
+function Transport:getConnection()
+  -- Function body
+end
+
+-------------------------------------------------------------------------------
+-- Makes a request to using a connection
+--
+-- @param   params  The parameters to be passed
+-- @return  table   The reponse returned
+-------------------------------------------------------------------------------
+function Transport:request(params)
+  -- Function body
+end
