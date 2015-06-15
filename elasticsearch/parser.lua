3d4ead96a61e9adbd8543e7721fe392284719833
Dhaval Kapil <me@dhavalkapil.com>
2015-06-16 01:42:06 +0530

    Updated parser module to use cjson

--- elasticsearch/parser.lua
+++ elasticsearch/parser.lua
@@ -1,26 +1,20 @@
 -------------------------------------------------------------------------------
+-- Importing module
+-------------------------------------------------------------------------------
+local cjson = require "cjson"
+-------------------------------------------------------------------------------
 -- Declaring module
 -------------------------------------------------------------------------------
 local parser = {}
 
 -------------------------------------------------------------------------------
 -- Encodes a table to a json string
---
--- @param   data    The data to encode
--- @return  string  The JSON as a string
 -------------------------------------------------------------------------------
-function parser.jsonEncode(data)
-  -- Function of body
-end
+parser.jsonEncode = cjson.encode
 
 -------------------------------------------------------------------------------
 -- Decodes a json string to a table
---
--- @param   json    The json string to be decoded
--- @return  table   The decoded table
 -------------------------------------------------------------------------------
-function parser.jsonDecode(json)
-  -- Function of body
-end
+parser.jsonDecode = cjson.decode
 
 return parser
