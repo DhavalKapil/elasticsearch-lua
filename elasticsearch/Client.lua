78bb4ccfa7fb3dc51ce3c592c10d8dddf19f1308
Dhaval Kapil <me@dhavalkapil.com>
2015-06-21 20:57:49 +0530

    Implemented basic info endpoint

--- elasticsearch/Client.lua
+++ elasticsearch/Client.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- Importing modules
 -------------------------------------------------------------------------------
-local Transport = require "Transport"
+local Settings = require "Settings"
 
 -------------------------------------------------------------------------------
 -- Declaring module
@@ -14,6 +14,31 @@ local Client = {}
 
 -- The Settings instance
 Client.settings = nil
+
+-------------------------------------------------------------------------------
+-- Function to get an endpoint instance for a particular type of request
+--
+-- @param   endpoint  The string denoting the endpoint
+--
+-- @return  Endpoint  The endpoint object
+-------------------------------------------------------------------------------
+function Client:getEndpoint(endpoint)
+  local Endpoint = require("endpoints." .. endpoint)
+  local endpoint = Endpoint:new{
+    transport = self.settings.transport
+  }
+  return endpoint
+end
+
+-------------------------------------------------------------------------------
+-- Function to get information regarding the Elasticsearch server
+--
+-- return   table     The details about the elasticsearch server as lua-table
+-------------------------------------------------------------------------------
+function Client:info()
+  local endpoint = self:getEndpoint("Info")
+  return endpoint:request().body
+end
 
 -------------------------------------------------------------------------------
 -- Returns an instance of Client class
