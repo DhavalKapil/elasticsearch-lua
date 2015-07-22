06ca7603e9fbd279678fce27520cc44d16a8eb43
Dhaval Kapil <me@dhavalkapil.com>
2015-07-23 05:17:30 +0530

    Fixed rockspec file and comments

--- elasticsearch/Client.lua
+++ elasticsearch/Client.lua
@@ -27,7 +27,7 @@ Client.nodes = nil
 --
 -- @param   endpoint        The string denoting the endpoint
 -- @param   params          The parameters to be passed
--- @params  endpointParams  The endpoint params passed while object creation
+-- @param   endpointParams  The endpoint params passed while object creation
 --
 -- @return  table     Error or the data recevied from the elasticsearch server
 -------------------------------------------------------------------------------
