9bb44f0617cbafaf05156f45997bbee4e3926499
Dhaval Kapil <me@dhavalkapil.com>
2015-07-31 23:17:16 +0530

    Added complete params details in comments for the indices module

--- elasticsearch/Indices.lua
+++ elasticsearch/Indices.lua
@@ -46,6 +46,8 @@ end
 -------------------------------------------------------------------------------
 -- Function to check whether an index exists or not
 --
+-- params["index"] = (list) A comma-separated list of indices to check (Required)
+--
 -- @param    params    The exists Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -67,6 +69,13 @@ end
 -------------------------------------------------------------------------------
 -- Function to get an index
 --
+-- params["index"] = (list) A comma-separated list of indices to check (Required)
+--       ["feature"] = (list) A comma-separated list of features to return
+--       ["ignore_unavailable"] = (bool) Whether specified concrete indices should be ignored when unavailable (missing or closed)
+--       ["allow_no_indices"]   = (bool) Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes '_all' string or when no indices have been specified)
+--       ["expand_wildcards"]   = (enum) Whether to expand wildcard expression to concrete indices that are open, closed or both.
+--       ["local"]   = (bool) Return local information, do not retrieve the state from master node (default: false)
+--
 -- @param    params    The get Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -77,6 +86,9 @@ end
 
 -------------------------------------------------------------------------------
 -- Function to delete indices
+--
+-- params["index"]   = (list) A comma-separated list of indices to delete; use '_all' or empty string to delete all indices
+--       ["timeout"] = (time) Explicit operation timeout
 --
 -- @param    params    The delete Parameters
 --
@@ -89,6 +101,10 @@ end
 -------------------------------------------------------------------------------
 -- Function to create an index
 --
+-- params["index"]   = (string) The name of the index (Required)
+--       ["timeout"] = (time) Explicit operation timeout
+--       ["body"]    = (time) Explicit operation timeout
+--
 -- @param    params    The create Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -99,6 +115,17 @@ end
 
 -------------------------------------------------------------------------------
 -- Function to optimize an index
+--
+-- params["index"]                = (list) A comma-separated list of index names; use '_all' or empty string to perform the operation on all indices
+--       ["flush"]                = (boolean) Specify whether the index should be flushed after performing the operation (default: true)
+--       ["max_num_segments"]     = (number) The number of segments the index should be merged into (default: dynamic)
+--       ["only_expunge_deletes"] = (boolean) Specify whether the operation should only expunge deleted documents
+--       ["operation_threading"]  = () TODO: ?
+--       ["refresh"]              = (boolean) Specify whether the index should be refreshed after performing the operation (default: true)
+--       ["wait_for_merge"]       = (boolean) Specify whether the request should block until the merge process is finished (default: true)
+--       ["ignore_unavailable"]   = (bool) Whether specified concrete indices should be ignored when unavailable (missing or closed)
+--       ["allow_no_indices"]     = (bool) Whether to ignore if a wildcard indices expression resolves into no concrete indices. (This includes '_all' string or when no indices have been specified)
+--       ["expand_wildcards"]     = (enum) Whether to expand wildcard expression to concrete indices that are open, closed or both.
 --
 -- @param    params    The optimize Parameters
 --
@@ -111,6 +138,9 @@ end
 -------------------------------------------------------------------------------
 -- Function to open an index
 --
+-- params["index"]   = (string) The name of the index (Required)
+--       ["timeout"] = (time) Explicit operation timeout
+--
 -- @param    params    The open Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -121,6 +151,9 @@ end
 
 -------------------------------------------------------------------------------
 -- Function to close an index
+--
+-- params["index"]   = (string) The name of the index (Required)
+--       ["timeout"] = (time) Explicit operation timeout
 --
 -- @param    params    The open Parameters
 --
@@ -133,6 +166,16 @@ end
 -------------------------------------------------------------------------------
 -- Function to perform analysis process on a text
 --
+-- params["index"]        = (string) The name of the index to scope the operation
+--       ["analyzer"]     = (string) The name of the analyzer to use
+--       ["field"]        = (string) Use the analyzer configured for this field (instead of passing the analyzer name)
+--       ["filters"]      = (list) A comma-separated list of filters to use for the analysis
+--       ["prefer_local"] = (boolean) With 'true', specify that a local shard should be used if available, with 'false', use a random shard (default: true)
+--       ["text"]         = (string) The text on which the analysis should be performed (when request body is not used)
+--       ["tokenizer"]    = (string) The name of the tokenizer to use for the analysis
+--       ["format"]       = (enum) Format of the output
+--       ["body"]         = (enum) Format of the output
+--
 -- @param    params    The analyze Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -144,6 +187,12 @@ end
 -------------------------------------------------------------------------------
 -- Function to get the status of an index
 --
+-- params["index"]               = (list) A comma-separated list of index names; use '_all' or empty string to perform the operation on all indices
+--       ["ignore_indices"]      = (enum) When performed on multiple indices, allows to ignore 'missing' ones
+--       ["operation_threading"] = () TODO: ?
+--       ["recovery"]            = (boolean) Return information about shard recovery
+--       ["snapshot"]            = (boolean) TODO: ?
+--
 -- @param    params    The status Parameters
 --
 -- @return   table     Error or the data recevied from the elasticsearch server
@@ -154,6 +203,8 @@ end
 
 -------------------------------------------------------------------------------
 -- Function to seal an index
+--
+-- params["index"]   = (string) The name of the index
 --
 -- @param    params    The seal Parameters
 --
