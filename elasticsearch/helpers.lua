10e881ae76f176c36aea5bc56e7fbf029dcb8c7a
Dhaval Kapil <me@dhavalkapil.com>
2015-08-12 22:07:03 +0530

    Added helpers.lua that supports reindexing feature

---
+++ elasticsearch/helpers.lua
@@ -0,0 +1,71 @@
+-------------------------------------------------------------------------------
+-- Declaring module
+-------------------------------------------------------------------------------
+local helpers = {}
+
+function helpers.reindex(sourceClient, sourceIndex, targetIndex, query,
+  targetClient, scroll)
+
+  -- Setting up query, target_client and scroll
+  query = query or {}
+  targetClient = targetClient or sourceClient
+  scroll = scroll or "1m"
+
+  -- Performing a search query
+  local data, err = sourceClient:search{
+    index = sourceIndex,
+    search_type = "scan",
+    scroll = scroll,
+    body = query
+  }
+
+  -- Checking for error in search query
+  if data == nil then
+    return nil, err
+  end
+
+  -- Performing a repetitive scroll queries
+  while true do
+    local scrollId = data["_scroll_id"]
+    data, err = sourceClient:scroll{
+      scroll_id = scrollId,
+      scroll = scroll
+    }
+
+    -- Checking for error in scroll query
+    if data == nil then
+      print(err)
+      return nil, err
+    end
+
+    -- If no more hits then break
+    if #data["hits"]["hits"] == 0 then
+      break
+    end
+
+    -- Bulk indexing the documents
+    local bulkBody = {}
+    for _, item in pairs(data["hits"]["hits"]) do
+      table.insert(bulkBody, {
+        index = {
+          _index = targetIndex,
+          _type = item["_type"],
+          _id = item["_id"]
+        }
+      })
+      table.insert(bulkBody, item["_source"])
+    end
+
+    data, err = targetClient:bulk{
+      index = targetIndex,
+      body = bulkBody
+    }
+
+    -- Checking for error in bulk request
+    if data == nil then
+      return nil, err
+    end
+  end
+end
+
+return helpers
