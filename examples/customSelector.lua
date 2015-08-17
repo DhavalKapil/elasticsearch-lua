0ea95cfeea635fc233af317d7427f39c92c45b93
Dhaval Kapil <me@dhavalkapil.com>
2015-08-17 22:58:10 +0530

    Added support for custom selectors

---
+++ examples/customSelector.lua
@@ -0,0 +1,31 @@
+package.path = package.path .. ";../elasticsearch/?.lua"
+
+-- Import Selector
+local Selector = require "selector.Selector"
+
+-- Create a custom instace
+local MySelector = Selector:new()
+
+-- Implement selectNext(connections) function for MySelector
+function MySelector:selectNext(connections)
+  -- Return a connection depending on your custom algorithm
+  return connection[1]
+end
+
+-- Implement the constructor function
+function MySelector:new(o)
+  o = o or {}
+  -- Custom initialization code related to your algorithm
+  -- End custom code
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+local elasticsearch = require "elasticsearch"
+
+local client = elasticsearch.client{
+  params = {
+    selector = MySelector
+  }
+}
