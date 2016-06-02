e9ed3d9576f3b875baa19c90d8eaaf27e72e116a
Dhaval Kapil <me@dhavalkapil.com>
2016-06-02 19:19:10 +0530

    Added Health, PendingTasks and PutSettings tests

---
+++ tests/endpoints/ClusterTest/PutSettingsTest.lua
@@ -0,0 +1,41 @@
+-- Importing modules
+local PutSettings = require "elasticsearch.endpoints.Cluster.PutSettings"
+local MockTransport = require "lib.MockTransport"
+local getmetatable = getmetatable
+
+-- Setting up environment
+local _ENV = lunit.TEST_CASE "tests.endpoints.ClusterTest.PutSettingsTest"
+
+-- Declaring local variables
+local endpoint
+local mockTransport = MockTransport:new()
+
+-- Testing the constructor
+function constructorTest()
+  assert_function(PutSettings.new)
+  local o = PutSettings:new()
+  assert_not_nil(o)
+  local mt = getmetatable(o)
+  assert_table(mt)
+  assert_equal(mt, mt.__index)
+end
+
+-- The setup function
+function setup()
+  endpoint = PutSettings:new{
+    transport = mockTransport
+  }
+end
+
+-- Testing request
+function requestTest()
+  mockTransport.method = "PUT"
+  mockTransport.uri = "/_cluster/settings"
+  mockTransport.params = {}
+  mockTransport.body = nil
+
+  endpoint:setParams{}
+
+  local _, err = endpoint:request()
+  assert_nil(err)
+end
--- tests/endpoints/ClusterTest/init.lua
+++ tests/endpoints/ClusterTest/init.lua
@@ -1,2 +1,5 @@
 -- Requiring all endpoint related test modules
 require "endpoints.ClusterTest.GetSettingsTest"
+require "endpoints.ClusterTest.HealthTest"
+require "endpoints.ClusterTest.PendingTasksTest"
+require "endpoints.ClusterTest.PutSettingsTest"
