fe488321a0f591c60518857020f874ff6734e420
Dhaval Kapil <me@dhavalkapil.com>
2016-06-18 19:48:58 +0530

    Added Bulk Integration Test

--- tests/integration/init.lua
+++ tests/integration/init.lua
@@ -1,1 +1,7 @@
+util = require "lib.util"
+dataset = require "integration.dataset.dataset"
+operations = require "integration.operations"
+
+-- Integration tests
 require "integration.BasicTest"
+require "integration.BulkTest"
