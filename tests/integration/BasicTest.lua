fa34167594a926dbbfd8b2a47caa2c5fefc547b1
Dhaval Kapil <me@dhavalkapil.com>
2016-06-18 19:48:42 +0530

    Shifted operations to operations.lua

--- tests/integration/BasicTest.lua
+++ tests/integration/BasicTest.lua
@@ -1,96 +1,27 @@
--- Tests:
---   info()
---   index()
---   get()
---   delete()
+--  Tests:
+--    info()
+--    index()
+--    get()
+--    delete()
+-- 
 
 -- Importing modules
-local elasticsearch = require "elasticsearch"
 local util = require "lib.util"
 
 -- Setting up environment
 local _ENV = lunit.TEST_CASE "tests.integration.BasicTest"
 
-local client  = elasticsearch.client()
-
-
-function info()
-  -- Testing info
-  local res, status = client:info()
-  assert_not_nil(res)
-  --assert_equal(200, status)
-end
-
-function index(data)
-  -- Indexing all data in elasticsearch
-  for i = 1, #data do
-    local res, status = client:index{
-      index = "test_index",
-      type = "my_type",
-      id = data[i]["id"],
-      body = data[i]
-    }
-    assert_not_nil(res)
-    assert_true(status == 200 or status == 201)
-  end
-end
-
-function getExistingDocuments(data)
-  -- Testing whether data is in elasticsearch by choosing random points 50 times
-  math.randomseed(os.time())
-  for i = 1, 50 do
-    local ii = math.random(1, #data)
-    local res, status = client:get{
-      index = "test_index",
-      type = "my_type",
-      id = data[i]["id"]
-    }
-    assert_not_nil(res)
-    assert_equal(200, status)
-    util.check(data[i], res._source)
-  end
-end
-
-function delete(data)
-  -- Deleting documents
-  for i = 1, #data do
-    local res, status = client:delete{
-      index = "test_index",
-      type = "my_type",
-      id = data[i]["id"],
-    }
-    assert_not_nil(res)
-    assert_equal(200, status)
-  end
-end
-
-function getNonExistingDocuments(data)
-  -- Testing whether data is in elasticsearch by choosing random points 50 times
-  math.randomseed(os.time())
-  for i = 1, 50 do
-    local i = math.random(1, #data)
-    local res, err = client:get{
-      index = "test_index",
-      type = "my_type",
-      id = data[i]["id"]
-    }
-    assert_nil(res)
-    assert_equal("ClientError: Invalid response code: 404", err)
-  end
-end
-
 function test()
-  local dataset = require "integration.dataset.dataset"
-  local dataset_1 = util.slice(dataset, 1, #dataset/2)
-  local dataset_2 = util.slice(dataset, #dataset/2 + 1, #dataset)
-  info()
-  index(dataset_1)
-  getNonExistingDocuments(dataset_2)
-  getExistingDocuments(dataset_1)
-  index(dataset_2)
-  delete(dataset_1)
-  getNonExistingDocuments(dataset_1)
-  getExistingDocuments(dataset_2)
-  delete(dataset_2)
-  getNonExistingDocuments(dataset_2)
+  local dataset_1 = util.slice(dataset, 1, math.floor(#dataset/2))
+  local dataset_2 = util.slice(dataset, math.floor(#dataset/2) + 1, #dataset)
+  operations.info()
+  operations.index(dataset_1)
+  operations.getNonExistingDocuments(dataset_2)
+  operations.getExistingDocuments(dataset_1)
+  operations.index(dataset_2)
+  operations.delete(dataset_1)
+  operations.getNonExistingDocuments(dataset_1)
+  operations.getExistingDocuments(dataset_2)
+  operations.delete(dataset_2)
+  operations.getNonExistingDocuments(dataset_2)
 end
---
+++ tests/integration/operations.lua
@@ -0,0 +1,123 @@
+-- A collection of individual operations that will be tested with other components
+
+-- Importing modules
+local elasticsearch = require "elasticsearch"
+
+-- Setting up environment
+local _ENV = lunit.TEST_CASE "tests.integration.operations"
+
+-- Declaring module
+local operations = {}
+
+local client  = elasticsearch.client()
+
+-- Defining test index and type
+local TEST_INDEX = "elasticsearch-lua-test-index"
+local TEST_TYPE = "elasticsearch-lua-test-type"
+
+function operations.info()
+  -- Testing info
+  local res, status = client:info()
+  assert_not_nil(res)
+  assert_equal(200, status)
+end
+
+function operations.index(data)
+  -- Indexing all data in elasticsearch
+  for i = 1, #data do
+    local res, status = client:index{
+      index = TEST_INDEX,
+      type = TEST_TYPE,
+      id = data[i]["id"],
+      body = data[i]
+    }
+    assert_not_nil(res)
+    assert_true(status == 200 or status == 201)
+  end
+end
+
+function operations.getExistingDocuments(data)
+  for i = 1, #data do
+    local res, status = client:get{
+      index = TEST_INDEX,
+      type = TEST_TYPE,
+      id = data[i]["id"]
+    }
+    assert_not_nil(res)
+    assert_equal(200, status)
+    util.check(data[i], res._source)
+  end
+end
+
+function operations.delete(data)
+  -- Deleting documents
+  for i = 1, #data do
+    local res, status = client:delete{
+      index = TEST_INDEX,
+      type = TEST_TYPE,
+      id = data[i]["id"],
+    }
+    assert_not_nil(res)
+    assert_equal(200, status)
+  end
+end
+
+function operations.getNonExistingDocuments(data)
+  for i = 1, #data do
+    local res, err = client:get{
+      index = TEST_INDEX,
+      type = TEST_TYPE,
+      id = data[i]["id"]
+    }
+    assert_nil(res)
+    assert_equal("ClientError: Invalid response code: 404", err)
+  end
+end
+
+function operations.bulkIndex(data)
+  -- Creating bulk body
+  local bulkBody = {}
+  for i = 1, #data do
+    -- Specifing that it is an index operation
+    bulkBody[#bulkBody + 1] = {
+      index = {
+        ["_index"] = TEST_INDEX,
+        ["_type"] = TEST_TYPE,
+        ["_id"] = data[i]["id"]
+      }
+    }
+    -- Actual body
+    bulkBody[#bulkBody + 1] = data[i]
+  end
+
+  -- Indexing all data in a single bulk operation
+  local res, status = client:bulk{
+    body = bulkBody
+  }
+  assert_not_nil(res)
+  assert_equal(200, status)
+end
+
+function operations.bulkDelete(data)
+  -- Creating bulk body
+  local bulkBody = {}
+  for i = 1, #data do
+    -- Specifying that it is a delete operation
+    bulkBody[#bulkBody + 1] = {
+      delete = {
+        ["_index"] = TEST_INDEX,
+        ["_type"] = TEST_TYPE,
+        ["_id"] = data[i]["id"]
+      }
+    }
+  end
+
+  -- Deleting all data in a single bulk operation
+  local res, status = client:bulk{
+    body = bulkBody
+  }
+  assert_not_nil(res)
+  assert_equal(200, status)
+end
+
+return operations;
