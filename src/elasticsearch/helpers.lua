b52cf97960686553e796be3e0459c38d365c9e5f
Dhaval Kapil <me@dhavalkapil.com>
2016-06-22 23:44:08 +0530

    Fixed helpers.reindex and added more options

--- src/elasticsearch/helpers.lua
+++ src/elasticsearch/helpers.lua
@@ -8,40 +8,47 @@
 local helpers = {}
 
 -------------------------------------------------------------------------------
--- Function to reindex
+-- Reindex all documents from one index to another index that satisfy
+-- a given query
 --
 -- @param    sourceClient    The source client
 -- @param    sourceIndex     The source index
 -- @param    targetIndex     The target index
 -- @param    query           Search query to filter data to be reindexed
+--                           query is for request body search
 -- @param    targetClient    The target client
 -- @param    scroll          Specify how long a consistent view of the index
 --                           should be maintained for scrolled search
+-- @param    scanParams      Additional params passed to scan
+-- @param    bulkParams      Additional params passed to bulk
+--
+-- @return   boolean         true if success, otherwise false
 -------------------------------------------------------------------------------
 function helpers.reindex(sourceClient, sourceIndex, targetIndex, query,
-  targetClient, scroll)
+  targetClient, scroll, scanParams, bulkParams)
 
-  -- Setting up query, target_client and scroll
+  -- Setting up default parameters
   query = query or {}
   targetClient = targetClient or sourceClient
   scroll = scroll or "1m"
+  scanParams = scanParams or {}
+  bulkParams = bulkParams or {}
 
   -- Performing a search query
-  local data, err = sourceClient:search{
-    index = sourceIndex,
-    search_type = "scan",
-    scroll = scroll,
-    body = query
-  }
+  scanParams.index = sourceIndex
+  scanParams.search_type = "scan"
+  scanParams.scroll = scroll
+  scanParams.body = query
+  local data, err = sourceClient:search(scanParams)
 
   -- Checking for error in search query
   if data == nil then
-    return nil, err
+    return false, err
   end
 
+  local scrollId = data["_scroll_id"]
   -- Performing a repetitive scroll queries
   while true do
-    local scrollId = data["_scroll_id"]
     data, err = sourceClient:scroll{
       scroll_id = scrollId,
       scroll = scroll
@@ -49,14 +56,15 @@ function helpers.reindex(sourceClient, sourceIndex, targetIndex, query,
 
     -- Checking for error in scroll query
     if data == nil then
-      print(err)
-      return nil, err
+      return false, err
     end
 
     -- If no more hits then break
     if #data["hits"]["hits"] == 0 then
       break
     end
+
+    scrollId = data["_scroll_id"]
 
     -- Bulk indexing the documents
     local bulkBody = {}
@@ -71,16 +79,16 @@ function helpers.reindex(sourceClient, sourceIndex, targetIndex, query,
       table.insert(bulkBody, item["_source"])
     end
 
-    data, err = targetClient:bulk{
-      index = targetIndex,
-      body = bulkBody
-    }
+    bulkParams.body = bulkBody
+
+    data, err = targetClient:bulk(bulkParams)
 
     -- Checking for error in bulk request
     if data == nil then
-      return nil, err
+      return false, err
     end
   end
+  return true
 end
 
 return helpers
