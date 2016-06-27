bd0e3b4f21afba2a4d95019582662839de429999
Dhaval Kapil <me@dhavalkapil.com>
2016-06-27 23:00:11 +0530

    Added max aggregation test

--- tests/integration/SearchTest.lua
+++ tests/integration/SearchTest.lua
@@ -7,8 +7,12 @@ local _ENV = lunit.TEST_CASE "tests.integration.SearchTest"
 
 function test()
   operations.bulkIndex(dataset)
+
+  -- Simple query search
   local res = operations.searchQuery("type:PushEvent")
   assert_equal(103, res.hits.total)
+
+  -- Simple body search
   local res = operations.searchBody{
     query = {
       match = {
@@ -17,5 +21,18 @@ function test()
     }
   }
   assert_equal(103, res.hits.total)
+
+  -- Aggregation search
+  local res = operations.searchBody{
+    aggs = {
+      maxActorId = {
+        max = {
+          field = "actor.id"
+        }
+      }
+    }
+  }
+  assert_equal(10364741.0, res.aggregations.maxActorId.value)
+
   operations.bulkDelete(dataset)
 end
