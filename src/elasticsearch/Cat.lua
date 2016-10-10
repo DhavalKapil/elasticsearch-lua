db81907daa59563a955496f0f8ad65128448c6e9
Dhaval Kapil <me@dhavalkapil.com>
2016-10-10 22:42:37 +0530

    Added Repositories endpoint

--- src/elasticsearch/Cat.lua
+++ src/elasticsearch/Cat.lua
@@ -299,6 +299,25 @@ function Cat:recovery(params)
 end
 
 -------------------------------------------------------------------------------
+-- Repositories function
+--
+-- @usage
+-- params["local"]          = (boolean) Return local information, do not retrieve the state from master node
+--       (default: false)
+--       ["master_timeout"] = (time) Explicit operation timeout for connection to master node
+--       ["h"]              = (list) Comma-separated list of column names to display
+--       ["help"]           = (boolean) Return help information (default: false)
+--       ["v"]              = (boolean) Verbose mode. Display column headers (default: false)
+--
+-- @param    params    The repositories Parameters
+--
+-- @return   table     Error or the data received from the elasticsearch server
+-------------------------------------------------------------------------------
+function Cat:repositories(params)
+  return self:requestEndpoint("Repositories", params)
+end
+
+-------------------------------------------------------------------------------
 -- Returns an instance of Cat class
 -------------------------------------------------------------------------------
 function Cat:new(o)
---
+++ src/elasticsearch/endpoints/Cat/Repositories.lua
@@ -0,0 +1,53 @@
+-------------------------------------------------------------------------------
+-- Importing modules
+-------------------------------------------------------------------------------
+local CatEndpoint = require "elasticsearch.endpoints.Cat.CatEndpoint"
+
+-------------------------------------------------------------------------------
+-- Declaring module
+-------------------------------------------------------------------------------
+local Repositories = CatEndpoint:new()
+
+-------------------------------------------------------------------------------
+-- Declaring Instance variables
+-------------------------------------------------------------------------------
+
+-- The parameters that are allowed to be used in params
+Repositories.allowedParams = {
+  ["local"] = true,
+  ["master_timeout"] = true,
+  ["h"] = true,
+  ["help"] = true,
+  ["v"] = true
+}
+
+-------------------------------------------------------------------------------
+-- Function to calculate the http request method
+--
+-- @return    string    The HTTP request method
+-------------------------------------------------------------------------------
+function Repositories:getMethod()
+  return "GET"
+end
+
+-------------------------------------------------------------------------------
+-- Function to calculate the URI
+--
+-- @return    string    The URI
+-------------------------------------------------------------------------------
+function Repositories:getUri()
+  local uri = "/_cat/repositories"  
+  return uri
+end
+
+-------------------------------------------------------------------------------
+-- Returns an instance of Repositories class
+-------------------------------------------------------------------------------
+function Repositories:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+return Repositories
