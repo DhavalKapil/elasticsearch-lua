46f0cab0f451680982aac59712738750b2a44541
Dhaval Kapil <me@dhavalkapil.com>
2016-10-26 22:22:18 +0530

    Updated lua version to 5.3.3 and es version to 2.4.1

--- .travis/install_es.sh
+++ .travis/install_es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o elasticsearch-latest-SNAPSHOT.zip https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.3/elasticsearch-2.3.3.zip
+curl -L -o elasticsearch-latest-SNAPSHOT.zip https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.4.1/elasticsearch-2.4.1.zip
 
 unzip "elasticsearch-latest-SNAPSHOT.zip"
 
